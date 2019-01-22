46ebd12a72d7bbaf280d097becefc09409cda0e4
Krithin Sitaram <krithin.sitaram@remix.com>
2019-01-22 11:44:06 -0800

    Fix the route_points query.

--- db/trips/route_points.sql
+++ db/trips/route_points.sql
@@ -1,14 +1,14 @@
 DROP MATERIALIZED VIEW IF EXISTS public.route_points CASCADE;
 
-WITH org_trips AS (
-    SELECT * FROM trips WHERE org_id = 11;
-)
-
 CREATE MATERIALIZED VIEW public.route_points AS
 
+WITH org_trips AS (
+    SELECT * FROM trips WHERE org_id = 11
+)
+
 SELECT
-    trips.provider_id,
-    trips.trip_id,
+    org_trips.provider_id,
+    org_trips.trip_id,
     coords.ts as time_point,
     coords.f as route_point,
     st_contains(csm_city_boundary(), csm_parse_feature_geom(coords.f)) as in_csm
@@ -16,7 +16,7 @@ FROM org_trips CROSS JOIN LATERAL (
     SELECT
         f,
         (f -> 'properties' ->> 'timestamp')::numeric as ts
-    FROM jsonb_array_elements(trips.route -> 'features') f
+    FROM json_array_elements(org_trips.route_geojson -> 'features') f
 ) coords
 ORDER BY
     provider_id,
